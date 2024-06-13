c2630f9af84134dccd3547e969a02d40d02bf8ce
Moizes <moizesdutra@gmail.com>
2024-06-13 16:18:55 -0300

    Testes_01

---
+++ Aula01/testes_01.py
@@ -0,0 +1,31 @@
+# minha_variavel_bonita = None
+
+# bruto = 1000
+# liquido = bruto - 100
+
+# inteiro = -46
+# num_decimal = 0.005
+# frase = "Está quente na sala"
+# verdadeiro = True
+# falso = False
+# nulo = None
+# frase = 45
+
+# a = 10
+# b = "10"
+# c = a + int(b)
+# print(c)
+
+# a = 10
+# b = "10"
+# c = str(a) + b
+# print(c)
+
+# print("Batman" + "Robin")
+
+# melao = 8
+# abobrinha = 4
+# resultado = melao / abobrinha
+
+numero = float(input("Digite um número: "))
+print("O dobro do número é", numero * 2)
