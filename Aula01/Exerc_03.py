6ca35b3ac75b8a0ac5b8e70872e95713cd8844fe
Moizes <moizesdutra@gmail.com>
2024-06-13 16:36:29 -0300

    Exercícios

---
+++ Aula01/Exerc_03.py
@@ -0,0 +1,3 @@
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite um número: "))
+print("A soma dos números é", numero_1 ++ numero_2 )
---
+++ Aula01/Exerc_05.py
@@ -0,0 +1,2 @@
+metros = float(input("Insira e medida em metros: "))
+print(metros, " Equivale a ", metros * 100, "centímetros")
