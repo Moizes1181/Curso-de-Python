e73ac5824e9a77acbc4356b0f9ba6d20478e9611
Moizes <moizesdutra@gmail.com>
2024-06-14 17:33:37 -0300

    Exercs

--- Exerc_14.py
+++ Exerc_14.py
@@ -2,7 +2,7 @@ limite_captura_peixe_kg = 50
 multa_kg_excendente = 4
 kg_pescados = float(input("Quantidade total de peixes pescados (em quilos)"))
 if kg_pescados <= 50:
-   print(f"{kg_pescados}Kg de peixe capturados está dentro do limite de {limite_captura_peixe_kg}kg perimitidos ")
+   print(f"{kg_pescados}Kg de peixe capturados está dentro do limite de {limite_captura_peixe_kg}kg perimitidos")
 else:
    print(f"{kg_pescados}Kg de peixe capturados ultrapassouo limite de {limite_captura_peixe_kg}kg perimitidos em {kg_pescados - limite_captura_peixe_kg} kg")
    print(f"Gerando uma multa de R$ {(kg_pescados - limite_captura_peixe_kg) * multa_kg_excendente:.2f}".replace('.', ','))
