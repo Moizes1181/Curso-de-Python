3bdce6f0c5c8dd6dba88dfdcebb85719aee35511
Moizes <moizesdutra@gmail.com>
2024-06-14 19:31:25 -0300

    exercs

--- Exerc_12.py
+++ Exerc_12.py
@@ -2,5 +2,4 @@ altura_em_cm= float(input("Digite sua altura em cm: "))
 altura = altura_em_cm / 100
 print(altura)
 peso_ideal = (72.7 * altura) - 58
-print("O seu peso ideal é", peso_ideal )
-print(f"O seu peso ideal é: {peso_ideal}")
+print(f"O seu peso ideal é: {peso_ideal:.2f}")
