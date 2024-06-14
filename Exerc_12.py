407bcd93163acf8a14fffc811fd62a4e629a53d8
Moizes <moizesdutra@gmail.com>
2024-06-14 17:15:45 -0300

    Exercs

--- Exerc_12.py
+++ Exerc_12.py
@@ -3,3 +3,4 @@ altura = altura_em_cm / 100
 print(altura)
 peso_ideal = (72.7 * altura) - 58
 print("O seu peso ideal é", peso_ideal )
+print(f"O seu peso ideal é: {peso_ideal}")
---
+++ Exerc_14.py
@@ -0,0 +1,9 @@
+limite_captura_peixe_kg = 50
+multa_kg_excendente = 4
+kg_pescados = float(input("Quantidade total de peixes pescados (em quilos)"))
+if kg_pescados <= 50:
+   print(f"{kg_pescados}Kg de peixe capturados está dentro do limite de {limite_captura_peixe_kg}kg perimitidos ")
+else:
+   kg_pescados_acima_limite = kg_pescados - limite_captura_peixe_kg
+   print(f"{kg_pescados}Kg de peixe capturados ultrapassouo limite de {limite_captura_peixe_kg}kg perimitidos em {kg_pescados - limite_captura_peixe_kg} kg")
+   print(f"Gerando uma multa de R$ {(kg_pescados - limite_captura_peixe_kg) * multa_kg_excendente:.2f}".replace('.', ','))
