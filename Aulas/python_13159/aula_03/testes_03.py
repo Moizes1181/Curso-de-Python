c732e6319fc30945685f2709c8e49284bfb95e49
Moizes <moizesdutra@gmail.com>
2024-06-17 14:21:44 -0300

    Exercs PY

---
+++ Aulas/python_13159/aula_03/testes_03.py
@@ -0,0 +1,27 @@
+# print(len("ABC"))
+
+# print(max(-9, 5, 7, 500, 478, -999))
+
+
+# abacate = ["Batman", 3.14, False, "Melancia", [15, "Teste"], 0]
+# abacate[2] = True
+# print(abacate)
+# print(5 in abacate)
+# print("Melancia" in abacate)
+
+# print("a" in "abcd")
+
+# abacate = ["Batman", 3.14, False, "Melancia", [15, "Teste"], 0]
+# abacate.append(51)
+# print(abacate)
+
+
+frutas = ["Maça", "Banana", "Laranja", "Banana", 0]
+# frutas.insert(0, "Melancia")
+# print(frutas)
+# frutas.remove("Banana")
+# print(frutas)
+frutas.pop(2)
+# ultima_fruta = frutas.pop()
+print(frutas)
+# print(ultima_fruta)
--- Exerc_14.py
+++ Exerc_14.py
@@ -6,3 +6,21 @@ if kg_pescados <= 50:
 else:
    print(f"{kg_pescados}Kg de peixe capturados ultrapassouo limite de {limite_captura_peixe_kg}kg perimitidos em {kg_pescados - limite_captura_peixe_kg} kg")
    print(f"Gerando uma multa de R$ {(kg_pescados - limite_captura_peixe_kg) * multa_kg_excendente:.2f}".replace('.', ','))
+
+
+
+peso_peixes = float(input("Peso dos peixes em kg: "))
+ 
+PESO_LIMITE = 50
+ 
+excesso = peso_peixes - PESO_LIMITE
+excesso = max(excesso, 0)
+ 
+MULTA_POR_KG_EXCESSO = 4
+ 
+multa = excesso * MULTA_POR_KG_EXCESSO
+ 
+print(f"Peso total: {peso_peixes} kg")
+print(f"Excesso: {excesso} kg")
+print(f"Multa: R$ {multa:.2f}")
+
